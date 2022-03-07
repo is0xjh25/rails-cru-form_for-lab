b5166cfc424def32c6572d3280a44154178237f4
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-03-07 12:19:37 +0800

    Update: Test passed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  get 'genre/create'
+
+  get 'genre/update'
+
+  get 'genre/show'
+
+  get 'artist/create'
+
+  get 'artist/update'
+
+  get 'artist/show'
+
+  get 'song/create'
+
+  get 'song/update'
+
+  get 'song/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs
+  resources :genres
+  resources :artists
 end
